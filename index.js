3e113de4e8392eab98d237a1968ee3f18d416c11
mikejeter <mjeter45@yahoo.com>
2020-03-12 01:50:32 -0400

    Writing endpoints DELETE and PUT

--- index.js
+++ index.js
@@ -40,10 +40,54 @@ server.post('/hubs', (req, res) => {
         });
 });
 
-server.put('/hubs/:id', (req, res) => {
+// D DELETE (CRUD)  /hubs/5
 
+server.delete('/hubs/:id', (req, res) => {
+    const {id} = req.params;
+
+    db.remove(id)
+        .then(deleted => {
+            if (deleted) {
+                res.status(204).end();
+                } else {
+                    res.status(404).json({success:false, message:'id not found'});
+                }
+        })
+        .catch(err => {
+            res.status(500).json({success:false, err});
+        });
+})
+
+server.put('/hubs/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    db.update(id, changes)
+        .then(updated => {
+            if (updated) {
+                res.status(200).json({success:true, updated});
+                } else {
+                    res.status(404).json({success:false, message:'id not found'});
+                }
+        })
+        .catch(err => {
+            res.status(500).json({success:false, err});
+        });
 })
 
 server.patch('/hubs/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
 
+    db.update(id, changes)
+        .then(updated => {
+            if (updated) {
+                res.status(200).json({success:true, updated});
+                } else {
+                    res.status(404).json({success:false, message:'id not found'});
+                }
+        })
+        .catch(err => {
+            res.status(500).json({success:false, err});
+        });
 })
